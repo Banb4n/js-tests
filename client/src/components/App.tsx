dec779e8d9d641c748b2ddf4e6d306e0f6022611
Banb4n <alban.such@gmail.com>
2019-06-20 15:20:28 +0200

    Initial commit 🚀

---
+++ client/src/components/App.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+
+export function App() {
+    const a = 2;
+    if (a === 2) {
+        return (
+            <div>
+                <h1>hello</h1>
+            </div>
+        );
+    }
+    return (
+        <div>
+            <h1>test</h1>
+            <p>d</p>
+        </div>
+    );
+}
---
+++ server.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import Bundler from 'parcel-bundler';
+
+const bundler = new Bundler('./client/src/index.html');
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.get('/api/levels', (req, res) => {
+    res.json({ levels: 'hello' });
+});
+
+app.get('/api/levels/:levelID/tests', (req, res) => {
+    res.json({ tests: 'world' });
+});
+
+app.use(bundler.middleware());
+app.listen(port, () => console.info(`Server listen on port:${port}`));
